83ef347cb2d24843239bc6fcb994e85be03a5018
Aleksandar Vujasinovic <aleksandar.vujasinov@gmail.com>
2019-07-30 19:03:28 +0200

    System Administrator hotels CRUD

--- src/app/hotels/hotel.service.ts
+++ src/app/hotels/hotel.service.ts
@@ -6,13 +6,25 @@ import {Hotel} from "../models-hotel/hotel";
 @Injectable()
 export class HotelService {
   private readonly hotelsUrl: string;
+  private readonly hotelsCreateUrl: string;
+  private readonly hotelsDeleteUrl: string;
 
   constructor(private http: HttpClient) {
     this.hotelsUrl = 'http://localhost:8080/api/hotels';
+    this.hotelsCreateUrl = 'http://localhost:8080/api/sa/hotels';
+    this.hotelsDeleteUrl = 'http://localhost:8080/api/sa/hotels/';
   }
 
   public findAll(): Observable<Hotel[]> {
     return this.http.get<Hotel[]>(this.hotelsUrl);
+  }
+
+  public create(hotel: Hotel): Observable<Hotel> {
+    return this.http.post<Hotel>(this.hotelsCreateUrl, hotel);
+  }
+
+  public delete(id: number) : Observable<Hotel> {
+    return this.http.delete<Hotel>(this.hotelsDeleteUrl + id);
   }
 
 }
--- src/app/system-administrator/system-administrator.component.ts
+++ src/app/system-administrator/system-administrator.component.ts
@@ -1,11 +1,43 @@
 import {Component, OnInit} from "@angular/core";
+import {Hotel} from "../models-hotel/hotel";
+import {HotelService} from "../hotels/hotel.service";
+import {Address} from "../models-hotel/address";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Room} from "../models-hotel/room";
 
 @Component({
   selector: 'system-administrator',
   templateUrl: './system-administrator.component.html'
 })
 export class SystemAdministratorComponent implements OnInit {
+  hotel: Hotel = new Hotel();
+  hotels: Hotel[];
+  url: string;
+
+  constructor(private hotelService: HotelService, private router: Router, private activatedRoute: ActivatedRoute) {
+    this.hotel.address = new Address();
+    this.hotel.rooms = [];
+    this.url = this.activatedRoute.snapshot.paramMap.get("id");
+  }
   ngOnInit(): void {
+    this.findAll();
   }
 
+  public findAll() {
+    this.hotelService.findAll().subscribe(data => {
+      this.hotels = data;
+    })
+  }
+
+  public create() {
+    this.hotelService.create(this.hotel).subscribe(value => {
+      this.findAll();
+    });
+  }
+
+  public delete(id: number) {
+    this.hotelService.delete(id).subscribe(value => {
+      this.findAll();
+    })
+  }
 }
